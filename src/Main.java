34d925cc603c90fc104448154021d53cfeca541f
Nastia <anastasiamalew@gmail.com>
2024-06-30 23:18:12 +0300

    HW11

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 
 public class Main {
 
-        public static void main (String[]args) {
+    public static void main(String[] args) {
         System.out.println("Задание 1");
         int yearToCheck = 2024;
         checkLeapYear(yearToCheck);
+        int deliveryDistance = 25;
+        distanceTime(deliveryDistance);
+        int deviceOs = 0;
+        int deviceYear = 2015;
+        checkerDeviceClient(deviceOs, deviceYear);
     }
 
-        public static void checkLeapYear ( int year){
+    public static void checkLeapYear(int year) {
         if ((year % 100 != 0 && year % 4 == 0) || (year % 400 == 0)) {
             System.out.println(year + " год - високосный");
         } else {
@@ -15,13 +20,9 @@ public class Main {
         }
     }
 
-    public static void task (String[] args) {
-            System.out.println("Задание 2");
-            int clientDeviceOs = 0;
-            int clientDeviceYear = 2015;
-            checkerDeviceClient(clientDeviceOs, clientDeviceYear);
-        }
-        public static void checkerDeviceClient(int deviceOS, int deviceYear) {
+
+    public static void checkerDeviceClient(int deviceOS, int deviceYear) {
+        System.out.println("Задание 2");
         if (deviceOS == 0) {
             if (deviceYear >= 2015) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
@@ -32,33 +33,24 @@ public class Main {
             if (deviceYear >= 2015) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             } else {
-                System.out.println("Установите обычную версию приложения для iOS по ссылке");
+                System.out.println("Установите обычную версию приложения для Android по ссылке");
             }
         } else {
             System.out.println("Выберете операционную систему вашего устройства");
         }
     }
+
     public static int distanceTime(int km) {
-    System.out.println("Задание 3");
-            if (km <= 20) {
-                return 1;
-            } else if (km > 20 && km < 60) {
-                return 2;
-            } else if (km >= 60 && km < 100) {
-                return 3;
-            } else {
-                // Тут можно возвращать специальное значение, например 0, что означает "доставки нет".
-                return 0;
-            }
+        System.out.println("Задание 3");
+        if (km <= 20) {
+            System.out.println("Потребуется дней:1" + " " + "срок доставки.");
+        } else if (km > 20 && km < 60) {
+            System.out.println("Потребуется дней:2" + " " + "срок доставки.");
+        } else if (km >= 60 && km < 100) {
+            System.out.println("Потребуется дней:3" + " " + "срок доставки.");
+        } else if (km >= 100) {
+            System.out.println("доставки нет.");
         }
-    public static void main(String[] args) {
-        int deliveryDistance = 50;
-        distanceTime(deliveryDistance);
-        int days = distanceTime(deliveryDistance);
-            if (days > 0) {
-                System.out.println("Потребуется дней: " + days + " срок доставки.");
-            } else {
-                System.out.println("Доставки нет.");
-            }
-        }
+        return km;
     }
+}
