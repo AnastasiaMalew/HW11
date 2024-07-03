2c4ee4fe20456deb83d51dd808bddc2cc866f364
Nastia <anastasiamalew@gmail.com>
2024-07-03 14:59:43 +0300

    HW11

--- src/Main.java
+++ src/Main.java
@@ -24,27 +24,26 @@ public class Main {
     public static void checkerDeviceClient(int deviceOS, int deviceYear) {
         System.out.println("Задание 2");
         if ((deviceOS == 0) && (deviceYear >= 2015)) {
-            { System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else if {
                 System.out.println("Установите обычную версию приложения для iOS по ссылке");
-            }
-        } else if (deviceOS == 1) {
-            if (deviceYear >= 2015) {
+            } else if ((deviceOS == 1) && (deviceYear >= 2015)) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             } else {
                 System.out.println("Установите обычную версию приложения для Android по ссылке");
+            } else if {
+                System.out.println("Выберете операционную систему вашего устройства");
             }
-        } else {
-            System.out.println("Выберете операционную систему вашего устройства");
         }
-    }
 
-    public static int distanceTime(int km) {
-        System.out.println("Задание 3");
-        if (km <= 20);
-        else if (km > 20 && km < 60);
-        else if (km >= 60 && km < 100);
-        else if (km >= 100);
-        return km;
+
+        public static int distanceTime ( int km){
+            System.out.println("Задание 3");
+            if (km <= 20) ;
+            else if (km > 20 && km < 60) ;
+            else if (km >= 60 && km < 100) ;
+            else if (km >= 100) ;
+            return km;
+        }
     }
 }
